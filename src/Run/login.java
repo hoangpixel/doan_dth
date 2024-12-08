5f7210d898d63a9fbb8dea30bc53b053c8ef44b5
hoangpixel <hoang.phy.pha.12@gmail.com>
2024-12-08 11:58:31 +0700

    fix logic login

--- src/Run/login.java
+++ src/Run/login.java
@@ -14,8 +14,19 @@ public class login {
         int d=0;
         do {
             System.out.println();
-            System.out.print("Nhập tài khoản : ");
-            taikhoan=sc.nextLine().trim();
+            while(true)
+            {
+                System.out.print("Nhập tài khoản : ");
+                taikhoan=sc.nextLine().trim();
+                if(taikhoan.matches("NV\\d{2}"))
+                {
+                    break;
+                }
+                else
+                {
+                    System.out.println("Vui lòng nhập tài khoản của nhân viên (NV..) !!!");
+                }
+            }
             System.out.print("Nhập mật khẩu : ");
             matkhau=sc.nextLine().trim();
             for(int i=0;i<nv.length;i++)
